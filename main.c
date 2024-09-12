69703fe145f250f507e96a589fe2841296967990
dudamarqs <eduardamarquesnoleto@gmail.com>
2024-09-11 23:39:26 -0300

    Chamando a função countByIdClass no main e adicionando mensagem para exibir o resultado

--- main.c
+++ main.c
@@ -11,8 +11,16 @@ int main()
 {
   Process processes[MAX_PROCESSES];
   int numProcesses = 0;
+  int id_classe_procurado = 11541;
+  int total; 
+
+  total = countByIdClass(processes, numProcesses, id_classe_procurado);
 
   readProcesses("processos.csv", processes, &numProcesses);
   selectionSortById(processes, numProcesses);
+  countByIdClass(processes, numProcesses, id_classe_procurado);
+
+  printf("Total de processos com id_classe %d: %d", id_classe_procurado, total);
+
   return 0;
 }
